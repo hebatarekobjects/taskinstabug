cd2455f1cdcd3e1ef8f3db72c29fdabf6d6e5912
Heba Tarek <heba.tarek@objects.ws>
2019-06-20 16:18:39 +0200

    controllers refactor

--- test/controllers/api/v1/messages_controller_test.rb
+++ test/controllers/api/v1/messages_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Api::V1::MessageControllerTest < ActionDispatch::IntegrationTest
+class Api::V1::MessagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
