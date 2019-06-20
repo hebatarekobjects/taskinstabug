8487d01b69f7628e2386cacf9af9a4f9e0662f1d
Heba Tarek <heba.tarek@objects.ws>
2019-06-20 16:07:18 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get "/chats", to:'chat#list'
-      get "/chats/:id", to: 'chat#show'
-      post "/chats", to: 'chat#new'
-      delete "/chats/:id", to: 'chat#destroy'
-      put "/chats/:id", to: 'chat#edit'
-      get "/applications", to:'application#list'
-      get "/applications/:id", to: 'application#show'
-      post "/applications", to: 'application#new'
-      delete "/applications/:id", to: 'application#destroy'
-      put "/applications/:id", to: 'application#edit'
-      get "/messages", to:'message#list'
-      get "/messages/:id", to: 'message#show'
-      post "/messages", to: 'message#new'
-      delete "/messages/:id", to: 'message#destroy'
-      put "/messages/:id", to: 'message#edit'
+      get "/chat", to:'chat#list'
+      get "/chat/:id", to: 'chat#show'
+      post "/chat", to: 'chat#new'
+      delete "/chat/:id", to: 'chat#destroy'
+      put "/chat/:id", to: 'chat#edit'
+      get "/application", to:'application#list'
+      get "/application/:id", to: 'application#show'
+      post "/application", to: 'application#new'
+      delete "/application/:id", to: 'application#destroy'
+      put "/application/:id", to: 'application#edit'
+      get "/message", to:'message#list'
+      get "/message/:id", to: 'message#show'
+      post "/message", to: 'message#new'
+      delete "/message/:id", to: 'message#destroy'
+      put "/message/:id", to: 'message#edit'
     end
   end
 end
