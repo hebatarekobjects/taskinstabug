557274808c75976e217c713b16443ab934032b80
Heba Tarek <heba.tarek@objects.ws>
2019-06-20 15:48:59 +0200

    update

---
+++ app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::ApplicationController < ApplicationController
+
+    def list
+    end
+    
+    def create
+    end
+
+    def new
+    end
+
+    def show :id
+    end
+
+    def edit :id
+    end
+
+    def update :id
+    end
+
+    def destroy :id
+    end
+end
---
+++ test/controllers/api/v1/message_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::V1::MessageControllerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
